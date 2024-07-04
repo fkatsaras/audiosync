b8b5360fe1caea4b4eea95a4e1906d36c3834f1a
fkatsaras <katsarasfotis99@gmail.com>
2024-07-04 16:37:08 +0300

    fixed home.py to render first html template in browser

--- app/__main__.py
+++ app/__main__.py
@@ -3,29 +3,20 @@
 import connexion
 
 from app import encoder
-from flask import Flask, Blueprint, jsonify
-from app.models.home_page_response import HomePageResponse, HomePageResponseLinks
-
-def create_app():
-    app = Flask(__name__, static_url_path='/static', static_folder='app/static')
-    app.config['SECRET_KEY'] = 'my_secret_key'
-
-    # Register blueprints here
-    from app.routes.home import home_bp
-    app.register_blueprint(home_bp)
-
-    return app
-
+from flask import Flask
+from app.routes.home import home_bp
 
 
 def main():
-    flask_app = create_app()
-
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Swagger AudioSync - OpenAPI 3.0'}, pythonic_params=True)
+
+    # Register the blueprint
+    flask_app = app.app
+    flask_app.register_blueprint(home_bp)
+
     app.run(port=8080)
-    flask_app.run(port=8080)
 
 
 if __name__ == '__main__':
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,21 +1,24 @@
-from flask import Blueprint, jsonify
-from app.models.home_page_response import HomePageResponse, HomePageResponseLinks
+from flask import Blueprint, render_template
+from app.models.home_page_response import HomePageResponse
+from app.models.home_page_response_links import HomePageResponseLinks
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def index():
+
+    BASE_URL = "http://localhost:8080"
     # Create an instance of HomePageResponseLinks with some dummy data
     links_data = HomePageResponseLinks(
-        liked_songs='/liked_songs',
-        recommended='/recommended',
-        my_artists='my_artists',
-        search='/search',
-        my_playlists='/my_playlists'
+        liked_songs=f'{BASE_URL}/liked_songs',
+        recommended=f'{BASE_URL}/recommended',
+        my_artists=f'{BASE_URL}/my_artists',
+        search=f'{BASE_URL}/search',
+        my_playlists=f'{BASE_URL}/my_playlists'
     )
 
     # Create an instance of HomePageResponse using the links data
     response_data = HomePageResponse(links=links_data)
 
     # Convert the response_data to a dictionary and jsonify it
-    return jsonify(response_data.to_dict())
+    return render_template('home.html', links_data=response_data.to_dict())
