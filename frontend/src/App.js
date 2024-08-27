1b6f429d2beee3f2cb12e3ef6565c0f5cca2b806
fkatsaras <katsarasfotis99@gmail.com>
2024-08-27 15:40:00 +0300

    Created Dockerfiles and ran containers for frontend/backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-// import './styles/App.css' // App wide styles
+// import './App.css';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/home" 
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />} 
+        />
+        <Route 
+          path="*"
+          element={<Navigate to="/" />}
+        />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
