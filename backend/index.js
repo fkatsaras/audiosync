5e10e325f1697a98b3c608354509a8c50cc33421
fkatsaras <katsarasfotis99@gmail.com>
2024-12-02 23:06:29 +0200

    Test deployment changes

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://localhost:4000', 'http://frontend:3000'],
+    origin: ['http://localhost:3000', 'https://localhost:4000', 'http://frontend:3000','http://audiosync.onrender.com'],
     credentials: true
 };
 
