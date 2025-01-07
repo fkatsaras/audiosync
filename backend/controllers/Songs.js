5beea06e399ea396fa5d916c978488502b7b9fce
fkatsaras <katsarasfotis99@gmail.com>
2025-01-07 14:03:39 +0200

    Fixed Disallow Undeclared Variables violation

--- backend/controllers/Songs.js
+++ backend/controllers/Songs.js
@@ -15,22 +15,3 @@ module.exports.get_song_by_id = function get_song_by_id (req, res, next) {
     });
 };
 
-module.exports.get_song_play_status = function get_song_play_status (req, res, next, songId) {
-  Songs.get_song_play_status(songId)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.toggle_song_playback = function toggle_song_playback (req, res, next, songId) {
-  Songs.toggle_song_playback(songId)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
