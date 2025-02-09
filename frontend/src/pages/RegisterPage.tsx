8a6c59d6a8c93ee51c9da108459fc4295f27d78b
fkatsaras <katsarasfotis99@gmail.com>
2025-02-09 23:26:54 +0200

    Frontend : fixed register styles

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -4,6 +4,7 @@ import Default from "../components/AppBody/Default";
 import Input from "../components/Input/Input";
 import Button from "../components/Buttons/Button";
 import Message from "../components/Message/Message";
+import '../styles/RegisterPage.css';
 
 function Register() {
     const [username, setUsername] = useState('');
@@ -76,62 +77,64 @@ function Register() {
     return (
         <Default>
             <div className="register-container">
-                <h1>Create Your Account</h1>
-                <form id="registerForm" onSubmit={handleRegister} className="register-form">
-                    <label htmlFor="username">Username:</label>
-                    <Input 
-                        type="text"
-                        id="username" 
-                        value={username} 
-                        onChange={(e) => setUsername(e.target.value)} 
-                        required
-                        placeholder="" 
-                    />
-                    <label htmlFor="email">Email:</label>
-                    <Input 
-                        type="email" 
-                        id="email" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
-                        required
-                        placeholder="" 
-                    />
-                    <label htmlFor="firstName">First Name:</label>
-                    <Input 
-                        type="text" 
-                        id="firstName" 
-                        value={firstName} 
-                        onChange={(e) => setFirstName(e.target.value)}
-                        placeholder="" 
-                    />
-                    <label htmlFor="lastName">Last Name:</label>
-                    <Input 
-                        type="text" 
-                        id="lastName" 
-                        value={lastName} 
-                        onChange={(e) => setLastName(e.target.value)}
-                        placeholder="" 
-                    />
-                    <label htmlFor="password">Password:</label>
-                    <Input 
-                        type="password" 
-                        id="password" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
-                        required
-                        placeholder="" 
-                    />
-                    <label htmlFor="confirmPassword">Confirm Password:</label>
-                    <Input 
-                        type="password" 
-                        id="confirmPassword" 
-                        value={confirmPassword} 
-                        onChange={(e) => setConfirmPassword(e.target.value)} 
-                        required
-                        placeholder="" 
-                    />
-                    <Button type="submit">Register</Button>
-                </form>
+                <div className="register-form">
+                    <h1>Create Your Account</h1>
+                    <form id="registerForm" onSubmit={handleRegister} className="register-form">
+                        <label htmlFor="username">Username:</label>
+                        <Input 
+                            type="text"
+                            id="username" 
+                            value={username} 
+                            onChange={(e) => setUsername(e.target.value)} 
+                            required
+                            placeholder="" 
+                        />
+                        <label htmlFor="email">Email:</label>
+                        <Input 
+                            type="email" 
+                            id="email" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            required
+                            placeholder="" 
+                        />
+                        <label htmlFor="firstName">First Name:</label>
+                        <Input 
+                            type="text" 
+                            id="firstName" 
+                            value={firstName} 
+                            onChange={(e) => setFirstName(e.target.value)}
+                            placeholder="" 
+                        />
+                        <label htmlFor="lastName">Last Name:</label>
+                        <Input 
+                            type="text" 
+                            id="lastName" 
+                            value={lastName} 
+                            onChange={(e) => setLastName(e.target.value)}
+                            placeholder="" 
+                        />
+                        <label htmlFor="password">Password:</label>
+                        <Input 
+                            type="password" 
+                            id="password" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required
+                            placeholder="" 
+                        />
+                        <label htmlFor="confirmPassword">Confirm Password:</label>
+                        <Input 
+                            type="password" 
+                            id="confirmPassword" 
+                            value={confirmPassword} 
+                            onChange={(e) => setConfirmPassword(e.target.value)} 
+                            required
+                            placeholder="" 
+                        />
+                        <Button type="submit">Register</Button>
+                    </form>
+                </div>
                 <p>Already have an account? <Link to="/login">Login here</Link></p>
                 {errorMessage && <Message className="error-message">{errorMessage}</Message>}
                 {successMessage && <Message className="success-message">{successMessage}</Message>}
